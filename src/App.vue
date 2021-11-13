d89bb484046d1c7ec62932553017a06c6576cf3f
azadpunchy <muhammadmananbari@gmail.com>
2021-11-13 17:24:46 +0500

    11/13/2021 today work

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,171 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div className="background_img"></div>
+  <div class="container">
+    <div class="main">
+      <div class="search">
+        <input
+          type="search"
+          v-model="city"
+          placeholder="&#128269; Search City"
+        />
+        <button @click="onclick()" class="btn">Search</button>
+      </div>
+      <!--   Search div Finished -->
+      <template v-if="show">
+        <div class="weatherIcon">
+          <img :src="toggleIcon" alt="clear day" width="60px" />
+          <span>{{ myImg.description }}</span>
+        </div>
+
+        <div class="temprature">
+          <h3>{{ res.temp }}&#8451;<!-- temp here --></h3>
+
+          <h2>
+            <span class="feel"
+              >City : {{ cityN }}<sup>`{{ countryN.country }}</sup></span
+            >
+          </h2>
+          <p>
+            <span>max : {{ res.temp_min }}&#8451; </span> || {{ " " }}
+            <span>min : {{ res.temp_max }}&#8451; </span>
+          </p>
+          <p>
+            <span class="feel"> Feel Like : {{ res.feels_like }}&#8451; </span
+            >{{ " " }} ||
+            <span class="feel">Humidity : {{ res.humidity }}&#8451;</span>
+          </p>
+        </div>
+      </template>
+      <template v-else> city not found </template>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Api from "./components/api/Api";
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      city: "faisalabad",
+      result: {},
+      toggleIcon: "",
+      alldata: {},
+      res: {},
+      countryN: {},
+      cityN: {},
+      myImg: {},
+      show: true,
+      // Icons data
+    };
+  },
+  methods: {
+    async load() {
+      try {
+        const URL = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=675a0dbdd9e7aaf0ec4db2b587eb87aa`;
+
+        this.city = "";
+        const response = await axios.get(URL);
+
+        // this.alldata = response;
+        console.warn(response.status);
+
+        this.res = response.data.main;
+        this.countryN = response.data.sys;
+        this.cityN = response.data.name;
+        // console.log("Api Data ", this.res);
+
+        if (response.status === 200) {
+          this.myImg = response.data.weather[0];
+
+          const icon = this.myImg.icon;
+          // console.log(icon);
+          this.show = true;
+
+          switch (icon) {
+            case "01d":
+              this.toggleIcon = Api.dclear;
+              break;
+            case "02d":
+              this.toggleIcon = Api.dfewClouds;
+              break;
+            case "03d":
+              this.toggleIcon = Api.cloudy;
+              break;
+            case "04d":
+              this.toggleIcon = Api.dbrokenClouds;
+              break;
+            case "09d":
+              this.toggleIcon = Api.showerRain;
+              break;
+            case "10d":
+              this.toggleIcon = Api.drain;
+              break;
+            case "11d":
+              this.toggleIcon = Api.dthunderStorm;
+              break;
+            case "13d":
+              this.toggleIcon = Api.dsnow;
+              break;
+            case "50d":
+              this.toggleIcon = Api.dmist;
+              break;
+            case "01n":
+              this.toggleIcon = Api.nclear;
+              break;
+            case "02n":
+              this.toggleIcon = Api.nfewClouds;
+              break;
+            case "03n":
+              this.toggleIcon = Api.cloudy;
+              break;
+            case "04n":
+              this.toggleIcon = Api.nbrokenClouds;
+              break;
+            case "09n":
+              this.toggleIcon = Api.showerRain;
+              break;
+            case "10n":
+              this.toggleIcon = Api.nrain;
+              break;
+            case "11n":
+              this.toggleIcon = Api.nthunderStorm;
+              break;
+            case "13n":
+              this.toggleIcon = Api.nsnow;
+              break;
+            case "50n":
+              this.toggleIcon = Api.nmist;
+              break;
+            default:
+              break;
+          }
+        } else {
+          console.log("i am from else");
+          this.show = false;
+        }
+
+        // console.log(this.toggleIcon);
+      } catch (err) {
+        console.log(err.message);
+      }
+    },
+    onclick() {
+      this.load();
+      this.alldata === 200 ? (this.show = true) : (this.show = false);
+      // this.toggle();
+    },
+  },
+  // components: {Api},
+  mounted() {
+    this.load();
+  },
+};
+
+// console.log(this.Api); //image api integrate it
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url("./style.css");
 </style>
---
+++ src/components/api/Api.js
@@ -0,0 +1,41 @@
+import dclear from "@/assets/images/all/01d.png";
+import dfewClouds from "@/assets/images/all/02d.png";
+import cloudy from "@/assets/images/all/03d.png"; //scatterd clouds for both
+import dbrokenClouds from "@/assets/images/all/02d.png";
+import showerRain from "@/assets/images/all/09d.png"; //for both
+import drain from "@/assets/images/all/10d.png";
+import dthunderStorm from "@/assets/images/all/11d.png";
+import dsnow from "@/assets/images/all/13d.png";
+import dmist from "@/assets/images/all/50d.png";
+//night
+import nclear from "@/assets/images/all/01n.png";
+import nfewClouds from "@/assets/images/all/02n.png";
+import nbrokenClouds from "@/assets/images/all/04n.png";
+import nrain from "@/assets/images/all/10n.png";
+import nthunderStorm from "@/assets/images/all/11n.png";
+import nsnow from "@/assets/images/all/13n.png";
+import nmist from "@/assets/images/all/50n.png";
+
+// icon: require("@/assets/chat.png");
+
+const Api = {
+  dclear,
+  dfewClouds,
+  cloudy,
+  dbrokenClouds,
+  showerRain,
+  drain,
+  dthunderStorm,
+  dsnow,
+  dmist,
+  // night
+  nclear,
+  nfewClouds,
+  nbrokenClouds,
+  nrain,
+  nthunderStorm,
+  nsnow,
+  nmist,
+};
+
+export default Api;
